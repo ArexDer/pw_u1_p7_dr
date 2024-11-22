a6c4d8195d8e3afc6cb47537af5572368996ea3d
Diego Rivas <darivas@uce.edu.ec>
2024-11-22 13:20:06 +0000

    completado toda la calculadora final

--- js/jscalculadorafinal.js
+++ js/jscalculadorafinal.js
@@ -1,68 +1,94 @@
- var numero1 =0 ;
- var numero2 =0 ;
- var operacion ='';
+// Variables globales
+var numero1 = 0;
+var numero2 = 0;
+var operacion = "";
+var acumulador = "";
+var expresion = ""; 
+
+
+document.getElementById("id-display").innerText = "00";
+
 
 function setearValor(valor) {
-
-  let valorActual = document.getElementById("id-display").innerText;
-  //Para cambiar el valor de un contenido TEXTO: con INNERTEXT.
-  console.log("El valor de entrada: " + valorActual);
-
   document.getElementById("id-display").innerText = valor;
 }
+
 
 function setearValorConcatenado(valor) {
   let valorActual = document.getElementById("id-display").innerText;
 
-  console.log("El valor de entrada: " + valor);
-
-  document.getElementById("id-display").innerText = valorActual + valor;
+ 
+  if (valorActual === "00") {
+    setearValor(valor);
+  } else {
+    setearValor(valorActual + valor); 
+  }
 }
 
 
-function recibirValor1(valor){
-    this.numero1=valor;
-    if(numero1===null){
-        numero1=valor;
-       
-    }else{
-        this.numero2=valor;
+function recibirValor1(valor) {
+  acumulador += valor; 
+  expresion += valor; 
+  setearValorConcatenado(valor); 
+}
+
+
+function evaluarOperacion(op) {
+  if (acumulador) {
+    if (numero1 === 0 && operacion === "") {
+      numero1 = parseFloat(acumulador); 
+    } else if (numero1 !== 0 && operacion !== "") {
+      numero2 = parseFloat(acumulador); 
+      realizarOperacion(); 
     }
-
-}
-
-function recibirValor2(valor){
-    this.numero2=valor;
-
-}
-
-function  evaluarOperacion(op){
-    this.operacion=op
-}
-
-function igual(){
-    let valorFinal;
-    if(operacion ==='+'){
-        valorFinal =numero1 + numero2;
-        setearValor(valorFinal);
-    }else if (operacion ==='-') {
-        valorFinal =numero1 - numero2;
-        setearValor(valorFinal);
-
-    }else if (operacion ==='x') {
-        valorFinal =numero1 * numero2;
-        setearValor(valorFinal);
-
-    }else if (operacion ==='/') {
-        valorFinal =numero1 / numero2;
-        setearValor(valorFinal);
-
-    }
+    operacion = op; 
+    acumulador = ""; 
+    expresion += op; 
+    setearValorConcatenado(op); 
+  }
 }
 
 
+function realizarOperacion() {
+  if (operacion === "+") {
+    numero1 = numero1 + numero2;
+  } else if (operacion === "-") {
+    numero1 = numero1 - numero2;
+  } else if (operacion === "*") {
+    numero1 = numero1 * numero2;
+  } else if (operacion === "/") {
+    numero1 = numero2 !== 0 ? numero1 / numero2 : "Error"; 
+  }
+  numero2 = 0;
+}
 
 
-    
-    
+function igual() {
+  if (acumulador && operacion) {
+    numero2 = parseFloat(acumulador); 
+    realizarOperacion(); 
+    operacion = ""; 
+    acumulador = numero1.toString(); 
+    setearValor(numero1); 
+    expresion = numero1.toString(); 
 
+  }
+}
+
+function borrar() {
+  if (expresion.length > 0) {
+    expresion = expresion.slice(0, -1); 
+    acumulador = acumulador.slice(0, -1); 
+    setearValor(expresion || "00"); 
+  }
+}
+
+
+function limpiar() {
+  numero1 = 0;
+  numero2 = 0;
+  operacion = "";
+  acumulador = "";
+  expresion = "";
+  setearValor("00"); 
+}
